0b139d4a880285b2a6bf7833d324e5afb3e700ba
rebeccaleifer <rebecca.leifer15@gmail.com>
2022-01-12 10:16:06 -0700

    Initial commit

---
+++ MyFirstASPNETWebApp/Controllers/Blah.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyFirstASPNETWebApp.Controllers
+{
+    public class Blah : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult FanMail()
+        {
+            return View();
+        }
+    }
+}
---
+++ MyFirstASPNETWebApp/wwwroot/js/FanMail.js
@@ -0,0 +1,13 @@
+﻿////$("#btnSend").addEventListener("click", function () {
+////    alert("From: " + $("#txtFrom").value +
+////        ", Subject: " + $("#txtSubject").value +
+////        ", Message: " + $("#txtMessage").value);
+////})
+
+$("#btnSend").click(function () {
+    alert("From: " + $("#txtFrom").val() +
+        ", Subject: " + $("#txtSubject").val() +
+        ", Message: " + $("#txtMessage").val());
+
+    $("#picGanderson").fadeOut("slow");
+})
